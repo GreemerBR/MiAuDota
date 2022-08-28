b2d509f264d4181c3f5e336877d78fe625d1a610
Gregory Viegas Zimmer <gregory.viegas.zimmer@hotmail.com>
2022-08-28 13:52:24 -0300

    Iniciando o repositório Foi criado o repositório e criada os projetos principais

---
+++ Entra21.TrabalhoFinal.Aplicacao/Entra21.TrabalhoFinal.Aplicacao/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Entra21.TrabalhoFinal.Aplicacao.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
