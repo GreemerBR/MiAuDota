336840586748d83335bebf917bf1c606910ec389
amanda.bauler <amanda.bauler@hotmail.com>
2022-08-29 21:29:01 -0300

    Criando "contexto.cs"

---
+++ Entra21.TrabalhoFinal.Aplicacao/Entra21.TrabalhoFinal.Repositorio/BancoDados/NomeIndefinidoContexto.cs
@@ -0,0 +1,12 @@
+﻿using Entra21.TrabalhoFinal.Repositorio.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entra21.TrabalhoFinal.Repositorio.BancoDados
+{
+    public class NomeIndefinidoContexto : DbContext
+    {
+        public DbSet<Animal> Animais { get; set; }
+        public DbSet<Protetor> Protetores { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
