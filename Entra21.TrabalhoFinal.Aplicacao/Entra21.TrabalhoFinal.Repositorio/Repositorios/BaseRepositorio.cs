53e0069f1b692f4543ef1cab93d8c3dac55dce38
Gregory Viegas Zimmer <gregory.v.zimmer@gmail.com>
2022-08-29 21:31:48 -0300

    Iniciando os repositorios

---
+++ Entra21.TrabalhoFinal.Aplicacao/Entra21.TrabalhoFinal.Repositorio/Repositorios/BaseRepositorio.cs
@@ -0,0 +1,34 @@
+﻿using Entra21.TrabalhoFinal.Repositorio.Entidades;
+
+namespace Entra21.TrabalhoFinal.Repositorio.Repositorios
+{
+    public class BaseRepositorio<T> : IRepositorio<T> where T : BaseModel
+    {
+        private readonly NomeIndefinidoContexto = _contexto;
+
+        public bool Apagar(int id)
+        {
+            using
+        }
+
+        public T Cadastrar(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Editar(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T? ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<T> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
