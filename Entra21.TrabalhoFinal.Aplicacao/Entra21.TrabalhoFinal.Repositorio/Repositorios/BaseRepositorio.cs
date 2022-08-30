86c18f31e32e4223525cf82bad0c057a5b3a7980
Gregory Viegas Zimmer <gregory.v.zimmer@gmail.com>
2022-08-29 22:06:57 -0300

    Criando o BaseRepositorio

--- Entra21.TrabalhoFinal.Aplicacao/Entra21.TrabalhoFinal.Repositorio/Repositorios/BaseRepositorio.cs
+++ Entra21.TrabalhoFinal.Aplicacao/Entra21.TrabalhoFinal.Repositorio/Repositorios/BaseRepositorio.cs
@@ -1,34 +1,73 @@
-﻿using Entra21.TrabalhoFinal.Repositorio.Entidades;
+﻿using Entra21.TrabalhoFinal.Repositorio.BancoDados;
+using Entra21.TrabalhoFinal.Repositorio.Entidades;
 
 namespace Entra21.TrabalhoFinal.Repositorio.Repositorios
 {
     public class BaseRepositorio<T> : IRepositorio<T> where T : BaseModel
     {
-        private readonly NomeIndefinidoContexto = _contexto;
+        //private readonly NomeIndefinidoContexto _contexto;
 
         public bool Apagar(int id)
         {
-            using
+            var entity = ObterPorId(id);
+
+            if (entity == null)
+            {
+                return false;
+            }
+            else
+            {
+                using (NomeIndefinidoContexto contexto = new NomeIndefinidoContexto())
+                {
+                    contexto.Entry<T>(entity).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
+                    contexto.SaveChanges();
+                }
+
+                return true;
+            }
         }
 
         public T Cadastrar(T entity)
         {
-            throw new NotImplementedException();
+            using (NomeIndefinidoContexto contexto = new NomeIndefinidoContexto())
+            {
+                contexto.Set<T>().Add(entity);
+                contexto.SaveChanges();
+            }
+
+            return entity;
         }
 
         public void Editar(T entity)
         {
-            throw new NotImplementedException();
+            using (NomeIndefinidoContexto contexto = new NomeIndefinidoContexto())
+            {
+                contexto.Entry<T>(entity).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            }
         }
 
         public T? ObterPorId(int id)
         {
-            throw new NotImplementedException();
+            T model = null;
+
+            using (NomeIndefinidoContexto contexto = new NomeIndefinidoContexto())
+            {
+                model = contexto.Set<T>().Find(id);
+            }
+
+            return model;
         }
 
         public IList<T> ObterTodos()
         {
-            throw new NotImplementedException();
+            IList<T> list = new List<T>();
+
+            using (NomeIndefinidoContexto contexto = new NomeIndefinidoContexto())
+            {
+                list = contexto.Set<T>().ToList();
+            }
+
+            return list;
         }
     }
 }
