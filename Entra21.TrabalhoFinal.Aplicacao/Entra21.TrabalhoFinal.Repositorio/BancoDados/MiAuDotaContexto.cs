a2ab169a98874c5d0edfae7e80270dcc0054f727
amanda.bauler <amanda.bauler@hotmail.com>
2022-08-31 19:40:34 -0300

    Mudado nome da classe contexto

--- Entra21.TrabalhoFinal.Aplicacao/Entra21.TrabalhoFinal.Repositorio/BancoDados/MiAuDotaContexto.cs
+++ Entra21.TrabalhoFinal.Aplicacao/Entra21.TrabalhoFinal.Repositorio/BancoDados/MiAuDotaContexto.cs
@@ -3,13 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Entra21.TrabalhoFinal.Repositorio.BancoDados
 {
-    public class NomeIndefinidoContexto : DbContext
+    public class MiAuDotaContexto : DbContext
     {
         public DbSet<Animal> Animais { get; set; }
         public DbSet<Protetor> Protetores { get; set; }
         public DbSet<Usuario> Usuarios { get; set; }
 
-        //public NomeIndefinidoContexto(DbSet<NomeIndefinidoContexto> options) : base(options)
+        //public MiAuDotaContexto(DbSet<NomeIndefinidoContexto> options) : base(options)
         //{
         //}
 
