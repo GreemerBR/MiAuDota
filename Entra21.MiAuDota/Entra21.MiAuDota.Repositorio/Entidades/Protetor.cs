66a288fece3bd82aacfa1fd80f30dbc335ab9f15
Samuel Lopes <samlowand@gmail.com>
2022-08-31 21:51:56 -0300

    Implementado/corrigido mapeamentos pendentes

--- Entra21.MiAuDota/Entra21.MiAuDota.Repositorio/Entidades/Protetor.cs
+++ Entra21.MiAuDota/Entra21.MiAuDota.Repositorio/Entidades/Protetor.cs
@@ -18,5 +18,7 @@ namespace Entra21.MiAuDota.Repositorio.Entidades
         public bool EhProtetor { get; set; }
 
         public StatusConta StatusConta { get; set; }
+
+        public IList<Animal> Animais { get; set; }
     }
 }
