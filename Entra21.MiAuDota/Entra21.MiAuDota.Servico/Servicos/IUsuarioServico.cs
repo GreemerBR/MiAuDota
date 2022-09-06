c681f745f7796ba4008708d4cbca4faa263cb530
amanda.bauler <amanda.bauler@hotmail.com>
2022-09-05 21:38:43 -0300

    .

--- Entra21.MiAuDota/Entra21.MiAuDota.Servico/Servicos/IUsuarioServico.cs
+++ Entra21.MiAuDota/Entra21.MiAuDota.Servico/Servicos/IUsuarioServico.cs
@@ -1,19 +1,14 @@
 ﻿using Entra21.MiAuDota.Repositorio.Entidades;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Entra21.MiAuDota.Servico.Servicos
 {
     public interface IUsuarioServico
     {
-        //Usuario Cadastrar(UsuarioCadastrarViewModel viewModel);
-        //bool Apagar(int id);
-        //bool Editar(UsuarioEditarViewModel viewModel);
-        //Usuario? ObterPorId(int id);
-        //IList<Usuario> ObterTodos(string pesquisa);
+        Usuario Cadastrar(UsuarioCadastrarViewModel viewModel);
+        bool Apagar(int id);
+        bool Editar(UsuarioEditarViewModel viewModel);
+        Usuario? ObterPorId(int id);
+        IList<Usuario> ObterTodos(string pesquisa);
 
     }
 }
