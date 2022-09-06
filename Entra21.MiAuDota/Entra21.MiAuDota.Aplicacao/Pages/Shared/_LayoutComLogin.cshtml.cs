7d8150f25a0d8e917370a662598b0cf13c6ee933
dbs07x <dbs07x@gmail.com>
2022-09-05 22:05:47 -0300

    Criado página _LayoutSemLogin e _LayoutComLogin

---
+++ Entra21.MiAuDota/Entra21.MiAuDota.Aplicacao/Pages/Shared/_LayoutComLogin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Entra21.MiAuDota.Aplicacao.Pages.Shared
+{
+    public class _LayoutComLoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
