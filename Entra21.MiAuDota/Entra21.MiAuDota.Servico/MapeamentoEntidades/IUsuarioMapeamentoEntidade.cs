c4bfdf808ff3dfb15c73c78f4b9ed020ed5ed304
Samuel Lopes <samlowand@gmail.com>
2022-09-05 21:41:15 -0300

    Serviço Usuario

---
+++ Entra21.MiAuDota/Entra21.MiAuDota.Servico/MapeamentoEntidades/IUsuarioMapeamentoEntidade.cs
@@ -0,0 +1,15 @@
+﻿using Entra21.MiAuDota.Repositorio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.MiAuDota.Servico.MapeamentoEntidades
+{
+    public interface IUsuarioMapeamentoEntidade
+    {
+        Usuario ConstruirCom(UsuarioCadastrarViewModel viewModel);
+        void AtualizarCampos(Usuario veterinario, UsuarioEditarViewModel viewModel);
+    }
+}
--- Entra21.MiAuDota/Entra21.MiAuDota.Servico/Servicos/IUsuarioServico.cs
+++ Entra21.MiAuDota/Entra21.MiAuDota.Servico/Servicos/IUsuarioServico.cs
@@ -9,11 +9,11 @@ namespace Entra21.MiAuDota.Servico.Servicos
 {
     public interface IUsuarioServico
     {
-        //Usuario Cadastrar(UsuarioCadastrarViewModel viewModel);
-        //bool Apagar(int id);
-        //bool Editar(UsuarioEditarViewModel viewModel);
-        //Usuario? ObterPorId(int id);
-        //IList<Usuario> ObterTodos(string pesquisa);
+        Usuario Cadastrar(UsuarioCadastrarViewModel viewModel);
+        bool Apagar(int id);
+        bool Editar(UsuarioEditarViewModel viewModel);
+        Usuario? ObterPorId(int id);
+        IList<Usuario> ObterTodos(string pesquisa);
 
     }
 }
