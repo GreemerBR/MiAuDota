2b8ad791e699705c219bb4d1d6941c55d73a4d17
Gregory Viegas Zimmer <gregory.v.zimmer@gmail.com>
2022-09-05 22:08:36 -0300

    .

---
+++ Entra21.MiAuDota/Entra21.MiAuDota.Servico/Servicos/BaseServico.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.MiAuDota.Servico.Servicos
+{
+    internal class BaseServico
+    {
+    }
+}
