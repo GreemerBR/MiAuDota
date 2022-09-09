61403ade48afdf75ebb423c32e7b64c4c67d6961
dbs07x <dbs07x@gmail.com>
2022-09-09 18:51:09 -0300

    Correção de erros e implementação das telas de login e cadastro

---
+++ Entra21.MiAuDota/Entra21.MiAuDota.Aplicacao/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Entra21.MiAuDota.Aplicacao.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
