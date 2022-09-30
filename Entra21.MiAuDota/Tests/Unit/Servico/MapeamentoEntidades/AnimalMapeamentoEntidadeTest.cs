b626e118c05f0a48d1d5c72bf31ea8b707b5ecda
samuel lopes <samlowand@gmail.com>
2022-09-29 22:12:12 -0300

    gerado teste unitario de animalmapeamento e protetorServico NADA FUNCIONANDO

--- Entra21.MiAuDota/Tests/Unit/Servico/MapeamentoEntidades/AnimalMapeamentoEntidadeTest.cs
+++ Entra21.MiAuDota/Tests/Unit/Servico/MapeamentoEntidades/AnimalMapeamentoEntidadeTest.cs
@@ -1,25 +1,82 @@
-﻿using Entra21.MiAuDota.Servico.Autenticacao;
+﻿using Entra21.MiAuDota.Repositorio.Entidades;
+using Entra21.MiAuDota.Repositorio.Enum;
+using Entra21.MiAuDota.Servico.Autenticacao;
 using Entra21.MiAuDota.Servico.MapeamentoEntidades;
+using Entra21.MiAuDota.Servico.ViewModels.Animais;
+using Entra21.MiAuDota.Servico.ViewModels.Protetores;
+using FluentAssertions;
 using NSubstitute;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Tests.Unit.Servico.MapeamentoEntidades
 {
-    public  class AnimalMapeamentoEntidadeTest
+    public class AnimalMapeamentoEntidadeTest
     {
         private readonly IAnimalMapeamentoEntidade _animalMapeamentoEntidade;
-        private readonly ISessionManager _session;
+        private readonly ISessionManager _sessionManager;
 
         public AnimalMapeamentoEntidadeTest()
         {
-            _session = Substitute.For<ISessionManager>();
-            _animalMapeamentoEntidade = new AnimalMapeamentoEntidade(_session);
+             _sessionManager = Substitute.For<ISessionManager>();
+            _animalMapeamentoEntidade = new AnimalMapeamentoEntidade(_sessionManager);
         }
 
+        [Fact]
+        public void Test_ConstruirCom()
+        {
+            // Arrange
+            var viewModel = new AnimalCadastrarViewModel
+            {
+                Nome = "Tobias",
+                Raca = "Dog",
+                Especie = "Cachorro",
+                Sobre = "Animal muito legal",
+                Vacinas = "Nenhuma",
+                Alergias = "Nehuma",
+                OutrasInformacoesMedicas = "Nada consta",
+                //Foto = "275E5840-F48D-4E7B-9156-D038C9AB89B4.jpg",
+                Idade = 2,
+                Peso = 2.2,
+                Altura = 0.5,
+                Castrado = false,
+                Genero = 1,
+                Status = 0,
+                UsuarioId = null,
+                ProtetorId = 1,
+
+                 DataAdocao = null
+                //DataAdocao = new DateTime(2020, 5, 11)
+
+            };
+
+            // Act
+            var animal = _animalMapeamentoEntidade.ConstruirCom(viewModel);
+
+            // Assert
+            animal.Nome.Should().Be(viewModel.Nome);
+            animal.Raca.Should().Be(viewModel.Raca);
+            animal.Especie.Should().Be(viewModel.Especie);
+            animal.Sobre.Should().Be(viewModel.Sobre);
+            animal.Vacinas.Should().Be(viewModel.Vacinas);
+            animal.Alergias.Should().Be(viewModel.Alergias);
+            animal.OutrasInformacoesMedicas.Should().Be(viewModel.OutrasInformacoesMedicas);
+            animal.Foto.Should().BeNull();
+            // animal.Foto.Should().Be(viewModel.Foto);
+            animal.Idade.Should().Be(viewModel.Idade);
+            animal.Peso.Should().Be(viewModel.Peso);
+            animal.Altura.Should().Be(viewModel.Altura);
+            animal.Castrado.Should().Be(viewModel.Castrado);
+            animal.Genero.Should().Be((GeneroAnimal)viewModel.Genero);
+            animal.Status.Should().Be((StatusInstituicao)viewModel.Status);
+            animal.UsuarioId.Should().BeNull();
+            animal.ProtetorId.Should().Be(viewModel.ProtetorId);
+            animal.DataAdocao.Should().BeNull();
+        }
 
     }
 }
---
+++ Entra21.MiAuDota/Tests/Unit/Servico/Servicos/ProtetorServicoTests.cs
@@ -0,0 +1,65 @@
+﻿using Entra21.MiAuDota.Repositorio.Entidades;
+using Entra21.MiAuDota.Repositorio.Repositorios;
+using Entra21.MiAuDota.Servico.MapeamentoEntidades;
+using Entra21.MiAuDota.Servico.MapeamentoViewModel;
+using Entra21.MiAuDota.Servico.Servicos;
+using Entra21.MiAuDota.Servico.ViewModels.Protetores;
+using FluentAssertions;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests.Unit.Servico.Servicos
+{
+    public class ProtetorServicoTests
+    {
+        private readonly IProtetorServico _protetorServico;
+        private readonly IProtetorRepositorio _protetorRepositorio;
+        private readonly IProtetorMapeamentoEntidade _protetorMapeamentoEntidade;
+        private readonly IProtetorMapeamentoViewModel _protetorMapeamentoViewModel;
+        public ProtetorServicoTests()
+        {
+            _protetorRepositorio = Substitute.For<IProtetorRepositorio>();
+            _protetorServico = new ProtetorServico(_protetorRepositorio, _protetorMapeamentoEntidade, _protetorMapeamentoViewModel);
+        }
+
+        [Fact]
+        public void Test_Cadastrar()
+        {
+            // Arrange
+            var viewModel = new ProtetorCadastrarViewModel
+            {
+                 Nome = "Ana",
+                Endereco = "Rua do mar",
+                Celular = "47999999999",
+                Telefone = "33302828",
+                Email = "ana@gmail.com",
+                Senha = "protetor123",
+                ConfirmarSenha = "protetor123",
+                Cpf = "12345678900",
+                Cnpj = "12.345.678/0001-00",
+                Pix = "ana@gmail.com",
+                Facebook = "@ana145",
+                Instagram = "@ana145",
+                Sobre = "abcdef"
+            };
+
+            // Act
+            _protetorServico.Cadastrar(viewModel);
+
+            // Assert
+            _protetorRepositorio.Received(1).Cadastrar(Arg.Is<Protetor>(
+                protetor => ValidarProtetor(protetor, viewModel)));
+        }
+        private bool ValidarProtetor(Protetor protetor, ProtetorCadastrarViewModel viewModel)
+        {
+            protetor.Nome.Should().Be(viewModel.Nome);
+
+            return true;
+        }
+    }
+}
