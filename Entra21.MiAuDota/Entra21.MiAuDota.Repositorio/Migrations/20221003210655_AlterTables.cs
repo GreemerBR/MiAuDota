6c4d5c49a6e088f6d546022baea9be09fd862d38
Gregory Viegas Zimmer <gregory.v.zimmer@gmail.com>
2022-10-05 18:17:13 -0300

    Nova migration

---
+++ Entra21.MiAuDota/Entra21.MiAuDota.Repositorio/Migrations/20221003210655_AlterTables.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entra21.MiAuDota.Repositorio.Migrations
+{
+    public partial class AlterTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "porte",
+                table: "animais",
+                type: "TINYINT",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "porte",
+                table: "animais");
+        }
+    }
+}
