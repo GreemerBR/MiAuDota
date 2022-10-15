81c1182b40488bf17a165f8aaae696485e9936e3
samuel l <samlowand@gmail.com>
2022-10-14 21:16:41 -0300

    validações nos campos de cadastro user e mascara cpf com js e controller retornando view

---
+++ Entra21.MiAuDota/Entra21.MiAuDota.Aplicacao/wwwroot/js/usuario/validacoesCadastrar.js
@@ -0,0 +1,36 @@
+﻿function mascara(i) {
+
+//    var cpf = document.getElementById('CPF');
+    var v = i.value;
+
+    if (isNaN(v[v.length - 1])) { 
+        i.value = v.substring(0, v.length - 1);
+        return;
+    }
+
+    i.setAttribute("maxlength", "14");
+    if (v.length == 3 || v.length == 7) i.value += ".";
+    if (v.length == 11) i.value += "-";
+
+}
+
+function TestaCPF(strCPF) {
+    var Soma;
+    var Resto;
+    Soma = 0;
+    if (strCPF == "00000000000") return false;
+
+    for (i = 1; i <= 9; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11)) Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10))) return false;
+
+    Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11)) Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11))) return false;
+    return true;
+}
