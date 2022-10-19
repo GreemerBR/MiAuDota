0ac72be8d0e955acd8ac6ad51a4b1dada3cd7cbd
Samuel.L <samlowand@gmail.com>
2022-10-18 22:12:18 -0300

    adicionado mascara telefone, celular cadastro protetor

--- Entra21.MiAuDota/Entra21.MiAuDota.Aplicacao/wwwroot/js/usuario/validacoesCadastrar.js
+++ Entra21.MiAuDota/Entra21.MiAuDota.Aplicacao/wwwroot/js/usuario/validacoesCadastrar.js
@@ -12,3 +12,17 @@
     if (v.length == 11) i.value += "-";
 
 }
+function mascaraTel(telefone) {
+
+    var v = telefone.value;
+
+    if (isNaN(v[v.length - 1])) {
+        telefone.value = v.substring(0, v.length - 1);
+        return;
+    }
+
+    telefone.setAttribute("maxlength", "12");
+    if (v.length == 2) telefone.value += " ";
+    if (v.length == 7) telefone.value += "-";
+}
+//bug de apagar depois do length 2
