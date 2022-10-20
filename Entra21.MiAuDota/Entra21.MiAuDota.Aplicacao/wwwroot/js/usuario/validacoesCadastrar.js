348fbf562fe2bf879ac92c1c2cb43fc99df84db2
Samuel.L <samlowand@gmail.com>
2022-10-19 22:07:32 -0300

    Mascaras de cadastro prontas!

--- Entra21.MiAuDota/Entra21.MiAuDota.Aplicacao/wwwroot/js/usuario/validacoesCadastrar.js
+++ Entra21.MiAuDota/Entra21.MiAuDota.Aplicacao/wwwroot/js/usuario/validacoesCadastrar.js
@@ -1,28 +1,4 @@
-﻿function mascara(i) {
-
-    var v = i.value;
-
-    if (isNaN(v[v.length - 1])) { 
-        i.value = v.substring(0, v.length - 1);
-        return;
-    }
-
-    i.setAttribute("maxlength", "14");
-    if (v.length == 3 || v.length == 7) i.value += ".";
-    if (v.length == 11) i.value += "-";
-
-}
-function mascaraTel(telefone) {
-
-    var v = telefone.value;
-
-    if (isNaN(v[v.length - 1])) {
-        telefone.value = v.substring(0, v.length - 1);
-        return;
-    }
-
-    telefone.setAttribute("maxlength", "12");
-    if (v.length == 2) telefone.value += " ";
-    if (v.length == 7) telefone.value += "-";
-}
-//bug de apagar depois do length 2
+﻿$('#Celular').mask('(00) 00000-0000');
+$('#Telefone').mask('(00) 0000-0000');
+$('#Cpf').mask('000.000.000-00', { reverse: true });
+$('#Cnpj').mask('00.000.000/0000-00', { reverse: true });
