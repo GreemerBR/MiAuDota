246c451ed866252729f6b86a1f7bbcb225016017
Gregory Viegas Zimmer <gregory.v.zimmer@gmail.com>
2022-10-23 12:14:22 -0300

    Editado modais e criado modal para o protetor

--- Entra21.MiAuDota/Entra21.MiAuDota.Aplicacao/wwwroot/js/protetor/protetor-acoes.js
+++ Entra21.MiAuDota/Entra21.MiAuDota.Aplicacao/wwwroot/js/protetor/protetor-acoes.js
@@ -6,6 +6,14 @@
     protetorAlterarStatus(element);
 });
 
+$('#animais').on('click', '.protetor-modalProtetor', (event) => {
+    let element = event.target.tagName === 'I'
+        ? event.target.parentElement
+        : event.target;
+
+    visualizarProtetorModal(element);
+});
+
 let protetorAlterarStatus = (element) => {
     debugger;
     let id = element.getAttribute("data-id");
@@ -30,3 +38,31 @@ let protetorAlterarStatus = (element) => {
             toastr.error('Não foi possível ativar a conta do protetor');
         })
 }
+
+let visualizarProtetorModal = (botaoProtetor) => {
+    let id = botaoProtetor.getAttribute('data-id');
+    let statusResponse = 0;
+
+    fetch(`/protetor/obterPorId?id=${id}`)
+        .then((response) => {
+            statusResponse = response.status;
+
+            return response.json();
+        })
+        .then((data) => {
+            if (statusResponse === 200) {
+                let modal = new bootstrap.Modal(document.getElementById('modalProtetor'), {});
+                
+                document.getElementById('modalProtetorLabel').innerText = `Protetor: ${data.nome}`
+                document.getElementById('modalProtetorId').value = data.id;
+                document.getElementById('modalProtetorEndereco').value = data.endereco;
+                document.getElementById('modalProtetorCelular').value = data.celular;
+                document.getElementById('modalProtetorTelefone').value = data.telefone;
+                document.getElementById('modalProtetorInstagram').value = data.instagram;
+                document.getElementById('modalProtetorFacebook').value = data.facebook;
+
+                modal.show();
+            }
+        })
+        .catch((error) => console.log(error));
+};
